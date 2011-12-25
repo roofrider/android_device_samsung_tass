409c749616f5c28202a6fca2568b09a76fca8ae2
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2011-12-25 21:20:31 +0700

    update files

---
+++ cm.mk
@@ -0,0 +1,18 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/tass/full_tass.mk)
+
+PRODUCT_NAME := cm_tass
+
+# Release name and versioning
+PRODUCT_RELEASE_NAME := GalaxyMini
+PRODUCT_VERSION_DEVICE_SPECIFIC :=
+-include vendor/cm/config/common_versions.mk
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := tass
